c14a2f0b9ab35a2b8b363a023a2e7a51678c1566
Dmitriy Sidenko <dmsidenko@gmail.com>
2020-01-08 23:12:20 +0300

    Add 1-4 part. Refactor README.md

--- app.py
+++ app.py
@@ -1,5 +1,51 @@
 import pandas
 
+
+def get_first_name(full_name):
+    try:
+        return full_name.split(',')[0]
+    except Exception as ex:
+        return None
+
+
+def get_middle_name(full_name):
+    try:
+        return full_name.split(',')[1].split('.')[0]
+    except Exception as ex:
+        return None
+
+
+def get_last_name(full_name):
+    try:
+        mn = full_name.split(',')[1].split('.')[0].strip()
+        if mn not in ['Mrs', 'Miss']:
+            return None
+        return full_name.split(',')[1].split('.')[1]
+    except Exception as ex:
+        return None
+
+
+def get_last_name2(full_name):
+    try:
+        fn = full_name.split(',')[1].split('.')[1]
+        if ' ' in fn:
+            return fn.split()[0]
+    except Exception as ex:
+        return None
+
+
+def get_last_name3(full_name):
+    try:
+        mn = full_name.split(',')[1].split('.')[0].strip()
+        fn = full_name.split(',')[1].split('.')[1]
+        if mn not in ['Mrs', 'Miss']:
+            return None
+        if ' ' in fn:
+            return fn.split()[0]
+    except Exception as ex:
+        return None
+
+
 if __name__ == '__main__':
     df = pandas.read_csv('titanic.csv', index_col='PassengerId')
     print(df.head())
@@ -29,3 +75,21 @@ if __name__ == '__main__':
     age_mean = data_age.mean()
     print("age_median", age_median)
     print("age_mean", age_mean)
+    # Corr Pirson
+    pir1 = df.corr()['Parch']['SibSp']
+    pir2 = df.corr()['SibSp']['Parch']
+    print(pir1, '   ', pir2)
+    # Name
+    data_name = df["Name"]
+    # df[['Last_name', 'First_name']]=data_name.str.split(',', expand=True)
+    # data_name.str.partition(',', True)
+    last_name_frame = data_name.apply(get_last_name)
+    last_name_frame = last_name_frame.dropna()
+    aa = [x.replace("((().,\"", "").split() for x in last_name_frame]
+    l = list()
+    for a in aa:
+        l.extend(a)
+    llll = pandas.Series(l)
+    # print(last_name_frame.value_counts()[:20])
+    print(llll.value_counts()[:40])
+
