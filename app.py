6224ad75f6d291346971da2d569bd60ed4f35545
Dmitriy Sidenko <dmsidenko@gmail.com>
2020-01-10 20:39:10 +0300

    Refactor code. Use DataFrame functions

--- app.py
+++ app.py
@@ -48,37 +48,31 @@ def get_last_name3(full_name):
 
 if __name__ == '__main__':
     df = pandas.read_csv('titanic.csv', index_col='PassengerId')
-    print(df.head())
+    # print(df.head())
     # data_sex = pandas.read_csv('titanic.csv', usecols=["Sex"], squeeze=True)
-
+    all_passenges_count = len(df)
     # Count number of males and females on the ship
-    data_sex = df["Sex"]
-    all_passenges_count = data_sex.count()
-    man_count = data_sex[data_sex.apply(lambda x: x == "male")].count()
-    woman_count = data_sex[data_sex.apply(lambda x: x == "female")].count()
+    # df = df.set_index("Sex")
+    # man_count = len(df.loc["male"])
+    # man_count = data_sex[data_sex.apply(lambda x: x == "male")].count()
+    # woman_count = len(df.loc["female"])
+    # woman_count = data_sex[data_sex.apply(lambda x: x == "female")].count()
     print("all_passenges_count", all_passenges_count)
-    print("man_count", man_count)
-    print("woman_count", woman_count)
+    print("man_count", len(df[df["Sex"] == "male"]))
+    print("woman_count", len(df[df["Sex"] == "female"]))
     # Survived_count
-    data_survived = df["Survived"]
-    survived_count = data_survived[data_survived.apply(lambda x: x == 1)].count()
+    survived_count = len(df[df["Survived"] == 1])
     print("survived_count", survived_count)
     print("survived_proc", 100*survived_count/all_passenges_count)
     # First class
-    data_class = df["Pclass"]
-    first_class_count = data_class[data_class.apply(lambda x: x == 1)].count()
+    first_class_count = len(df[df["Pclass"] == 1])
     print("first_class_count", first_class_count)
     print("first_class_proc", 100*first_class_count/all_passenges_count)
     # Age
-    data_age = df["Age"]
-    age_median = data_age.median()
-    age_mean = data_age.mean()
-    print("age_median", age_median)
-    print("age_mean", age_mean)
+    print("age_median",  df["Age"].median())
+    print("age_mean", df["Age"].mean())
     # Corr Pirson
-    pir1 = df.corr()['Parch']['SibSp']
-    pir2 = df.corr()['SibSp']['Parch']
-    print(pir1, '   ', pir2)
+    print(f"pir_corr by Parch and SibSp {df.corr()['SibSp']['Parch']}")
     # Name
     data_name = df["Name"]
     # df[['Last_name', 'First_name']]=data_name.str.split(',', expand=True)
