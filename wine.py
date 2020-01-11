cdcfcaf02c9d72902a7330d9a7bb6bcdc40ca5fb
Dmitriy Sidenko <dmsidenko@gmail.com>
2020-01-11 18:45:54 +0300

    Add cross-validation with normalization. Second part works

--- wine.py
+++ wine.py
@@ -6,9 +6,8 @@ import pandas
 
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn import tree
+from sklearn.model_selection import cross_val_score,  KFold
+from sklearn.preprocessing import scale
 import matplotlib.pyplot as plt
 
 WINE_HEADERS = [
@@ -32,23 +31,28 @@ if __name__ == '__main__':
     df = pandas.read_csv('wine.data', names=WINE_HEADERS)
     y = df['Class']
     del df['Class']
+    scaled_df = scale(df)
+    show_plots = False
 
-    best_result = 0
-    best_result_i = 0
-    best_results = []
+    for X in [df, scaled_df]:
+        best_result = 0
+        best_result_i = 0
+        best_results = []
 
-    for k in range(1, 51):
-        model = KNeighborsClassifier(n_neighbors=k)
-        model.fit(df, y)
-        kf = KFold(n_splits=5, shuffle=True, random_state=42)
-        results = cross_val_score(model, df, y, cv=kf, scoring="accuracy")
-        results_avg = np.average(results)
-        best_results.append(results_avg)
-        if results_avg > best_result:
-            best_result = results_avg
-            best_result_i = k
+        for k in range(1, 51):
+            model = KNeighborsClassifier(n_neighbors=k)
+            model.fit(X, y)
+            kf = KFold(n_splits=5, shuffle=True, random_state=42)
+            results = cross_val_score(model, X, y, cv=kf, scoring="accuracy")
+            results_avg = np.average(results)
+            best_results.append(results_avg)
+            if results_avg > best_result:
+                best_result = results_avg
+                best_result_i = k
 
-    best_result_np = np.array(best_results).max()
-    print(best_result, best_result_i, best_result_np)
-    plt.plot(best_results)
-    plt.show()
+        print(best_result, best_result_i)
+        if show_plots:
+            plt.plot(best_results)
+            plt.show()
+            # Close plot to continue
+
