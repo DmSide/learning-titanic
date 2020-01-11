bdf68c397bba523b331996e16c3e50d354af7a8c
Dmitriy Sidenko <dmsidenko@gmail.com>
2020-01-11 18:30:13 +0300

    Add cross-validation without normalization. First part works

--- wine.py
+++ wine.py
@@ -1,7 +1,13 @@
+# Analizing of data
+# https://archive.ics.uci.edu/ml/datasets/Wine
+# usuing k-neigbors Classifier
+# https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
 import pandas
 
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 from sklearn import tree
 import matplotlib.pyplot as plt
 
@@ -24,4 +30,25 @@ WINE_HEADERS = [
 
 if __name__ == '__main__':
     df = pandas.read_csv('wine.data', names=WINE_HEADERS)
-    print(df)
+    y = df['Class']
+    del df['Class']
+
+    best_result = 0
+    best_result_i = 0
+    best_results = []
+
+    for k in range(1, 51):
+        model = KNeighborsClassifier(n_neighbors=k)
+        model.fit(df, y)
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
+        results = cross_val_score(model, df, y, cv=kf, scoring="accuracy")
+        results_avg = np.average(results)
+        best_results.append(results_avg)
+        if results_avg > best_result:
+            best_result = results_avg
+            best_result_i = k
+
+    best_result_np = np.array(best_results).max()
+    print(best_result, best_result_i, best_result_np)
+    plt.plot(best_results)
+    plt.show()
