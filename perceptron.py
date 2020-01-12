55358bda3506c303391dc54c7dc851b457b4e6f0
Dmitriy Sidenko <dmsidenko@gmail.com>
2020-01-12 12:26:43 +0300

    No errors, but dont work

--- perceptron.py
+++ perceptron.py
@@ -3,13 +3,18 @@ import pandas
 from sklearn.linear_model import Perceptron
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+from sklearn_pandas import DataFrameMapper
+
 if __name__ == '__main__':
     df_train = pandas.read_csv('perceptron-train.csv', names=['c1', 'c2', 'c3'])
     df_test = pandas.read_csv('perceptron-test.csv', names=['c1', 'c2', 'c3'])
-    y_train = df_train.iloc[:,[0]]
-    X_train = df_train.iloc[:,1:]
-    y_test = df_test.iloc[:, [0]]
-    X_test = df_test.iloc[:, 1:]
+    y_train = df_train['c1']
+    X_train = df_train.copy()
+    del X_train['c1']
+    y_test = df_test['c1']
+    X_test = df_test.copy()
+    del X_test['c1']
+
     clf = Perceptron(random_state=241)
     clf.fit(X_train, y_train)
     y_pred_test = clf.predict(X_test)
@@ -17,20 +22,21 @@ if __name__ == '__main__':
     ans1 = accuracy_score(y_test, y_pred_test)
     print(ans1)
 
-    mapper = pandas.DataFrameMapper([(df_train.columns, StandardScaler())])
+    mapper = DataFrameMapper([(X_train.columns, StandardScaler())])
 
-    df_train_features = mapper.fit_transform(df_train.copy(), 4)
-    scaled_df_train = pandas.DataFrame(df_train_features, index=df_train.index, columns=df_train.columns)
+    X_train_features = mapper.fit_transform(X_train.copy(), 4)
+    scaled_X_train = pandas.DataFrame(X_train_features, index=X_train.index, columns=X_train.columns)
 
-    df_test_features = mapper.fit_transform(df_test.copy(), 4)
-    scaled_df_test = pandas.DataFrame(df_test_features, index=df_test.index, columns=df_test.columns)
+    X_test_features = mapper.fit_transform(X_test.copy(), 4)
+    scaled_X_test = pandas.DataFrame(X_test_features, index=X_test.index, columns=X_test.columns)
 
-    y_train = scaled_df_train.iloc[:, [0]]
-    X_train = scaled_df_train.iloc[:, 1:]
-    y_test = scaled_df_test.iloc[:, [0]]
-    X_test = scaled_df_test.iloc[:, 1:]
+    # y_train =
+    # np.asarray(scaled_df_train['c1'], dtype="|S6")  # scaled_df_train.iloc[:, [0]].astype(float)
+    X_train = scaled_X_train
+    #  y_test = np.asarray(scaled_df_test['c1'], dtype="|S6")  # scaled_df_test.iloc[:, [0]].astype(float)
+    X_test = scaled_X_test
 
-    clf = Perceptron(random_state=241)
+    # clf = Perceptron(random_state=241)
     clf.fit(X_train, y_train)
     y_pred_test = clf.predict(X_test)
 
