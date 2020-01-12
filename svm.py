12383fa80e010961266bff9de6fa7bf7b1639b0b
Dmitriy Sidenko <dmsidenko@gmail.com>
2020-01-12 16:26:42 +0300

    SVM method init

---
+++ svm.py
@@ -0,0 +1,11 @@
+import pandas
+import numpy as np
+from sklearn.svm import SVC
+
+if __name__ == '__main__':
+    df = pandas.read_csv('svm-data.csv', header=None)
+    y = df.loc[:, 0]
+    X = df.loc[:, 1:2]
+    sv = SVC(C=100000, random_state=241)
+    ans = sv.fit(X, y)
+    print(ans)
